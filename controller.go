ffb7e15533841146e5d8721b59c8497d4e615d40
liudng <liudng@gmail.com>
2015-07-25 16:56:12 +0200

    CRUD

--- controller.go
+++ controller.go
@@ -22,45 +22,85 @@ type Controller struct {
 
 // Add
 func (c *Controller) Add() interface{} {
-	var d map[string]interface{}
-	err := json.NewDecoder(c.Request.HTTPRequest.Body).Decode(&d)
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+		r, err := s.InsertInto(tbName).Exec(jsonPost)
+		if err != nil {
+			return Result{-2, err.Error()}
+		}
+
+		return Result{r.LastInsertId, ""}
+	})
+}
+
+// Detail
+func (c *Controller) Detail(key string) interface{} {
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+		d := db.Item{}
+		q := s.NewQuery()
+		err := q.Select("*").From(tbName).Where(q.Eq(primary, key)).Row(&d)
+		if err != nil {
+			return Result{-2, err.Error()}
+		}
+
+		return Result{d, ""}
+	})
+}
+
+// Edit
+func (c *Controller) Edit(key string) interface{} {
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+		w := db.Where{primary: key}
+		r, err := s.Update(tbName).Exec(jsonPost, w)
+		if err != nil {
+			return Result{-2, err.Error()}
+		}
+
+		return Result{r.RowsAffected, ""}
+	})
+}
+
+// Delete
+func (c *Controller) Delete(key string) interface{} {
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+		w := db.Where{primary: key}
+		r, err := s.DeleteFrom(tbName).Exec(w)
+		if err != nil {
+			return Result{-2, err.Error()}
+		}
+
+		return Result{r.RowsAffected, ""}
+	})
+}
+
+// List
+func (c *Controller) List(pi, ps int64) interface{} {
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+		d := []db.Item{}
+		q := s.NewQuery()
+		err := q.Select("*").From(tbName).Rows(&d)
+		if err != nil {
+			return Result{-3, err.Error()}
+		}
+
+		return Result{d, ""}
+	})
+}
+
+// CRUD 
+func (c *Controller) CRUD(fn func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}) interface{} {
+	jsonPost := db.Item{}
+	err := json.NewDecoder(c.Request.HTTPRequest.Body).Decode(&jsonPost)
 	if err != nil {
 		return Result{-1, err.Error()}
 	}
 
 	if s, ok := db.Servers[App.Modules[c.Request.Module].DB.Name]; ok {
 		tbName := console.CamelcaseToUnderscore(c.Request.Controller)
-		r, err := s.InsertInto(tbName).Exec(d)
-		if err != nil {
-			return Result{-2, err.Error()}
-		}
-
-		return Result{r.LastInsertId, ""}
+		primary := tbName+"_id"
+		return fn(jsonPost, s, tbName, primary)
 	} else {
-		return Result{-1, "No database configuration"}
+		return Result{-2, "No database configuration"}
 	}
-
-	return d
-}
-
-// Detail
-func (c *Controller) Detail(id string) interface{} {
-	return map[string]string{"code": "1", "err": "Detail: Wrong paramaters"}
-}
-
-// Update
-func (c *Controller) Edit(id string) interface{} {
-	return map[string]string{"code": "1", "err": "Edit: Wrong paramaters"}
-}
-
-// Delete
-func (c *Controller) Delete(id string) interface{} {
-	return map[string]string{"code": "1", "err": "Delete: Wrong paramaters"}
-}
-
-// List
-func (c *Controller) List(pi, ps int64) interface{} {
-	return map[string]string{"code": "1", "err": "List: Wrong paramaters"}
 }
 
 //new router register
