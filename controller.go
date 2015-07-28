5aac6ff147de6409a6921b4bd389cca8fc58c40a
liudng <liudng@gmail.com>
2015-07-28 17:35:01 +0800

    Auto format Controller.go

--- controller.go
+++ controller.go
@@ -22,10 +22,10 @@ type Controller struct {
 
 // Add
 func (c *Controller) Add() interface{} {
-	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{} {
 		r, err := s.InsertInto(tbName).Exec(jsonPost)
 		if err != nil {
-			return Result{-2, err.Error()}
+			return Result{-600, err.Error()}
 		}
 
 		return Result{r.LastInsertId, ""}
@@ -34,12 +34,12 @@ func (c *Controller) Add() interface{} {
 
 // Detail
 func (c *Controller) Detail(key string) interface{} {
-	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{} {
 		d := db.Item{}
 		q := s.NewQuery()
 		err := q.Select("*").From(tbName).Where(q.Eq(primary, key)).Row(&d)
 		if err != nil {
-			return Result{-2, err.Error()}
+			return Result{-600, err.Error()}
 		}
 
 		return Result{d, ""}
@@ -48,11 +48,11 @@ func (c *Controller) Detail(key string) interface{} {
 
 // Edit
 func (c *Controller) Edit(key string) interface{} {
-	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{} {
 		w := db.Where{primary: key}
 		r, err := s.Update(tbName).Exec(jsonPost, w)
 		if err != nil {
-			return Result{-2, err.Error()}
+			return Result{-600, err.Error()}
 		}
 
 		return Result{r.RowsAffected, ""}
@@ -61,11 +61,11 @@ func (c *Controller) Edit(key string) interface{} {
 
 // Delete
 func (c *Controller) Delete(key string) interface{} {
-	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{} {
 		w := db.Where{primary: key}
 		r, err := s.DeleteFrom(tbName).Exec(w)
 		if err != nil {
-			return Result{-2, err.Error()}
+			return Result{-600, err.Error()}
 		}
 
 		return Result{r.RowsAffected, ""}
@@ -74,36 +74,36 @@ func (c *Controller) Delete(key string) interface{} {
 
 // List
 func (c *Controller) List(pi, ps int64) interface{} {
-	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}{
+	return c.CRUD(func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{} {
 		d := []db.Item{}
 		q := s.NewQuery()
 		err := q.Select("*").From(tbName).Rows(&d)
 		if err != nil {
-			return Result{-3, err.Error()}
+			return Result{-600, err.Error()}
 		}
 
 		return Result{d, ""}
 	})
 }
 
-// CRUD 
+// CRUD
 func (c *Controller) CRUD(fn func(jsonPost db.Item, s *db.Server, tbName string, primary string) interface{}) interface{} {
 	jsonPost := db.Item{}
 	err := json.NewDecoder(c.Request.HTTPRequest.Body).Decode(&jsonPost)
 	if err != nil {
-		return Result{-1, err.Error()}
+		return Result{-100, err.Error()}
 	}
 
 	if s, ok := db.Servers[App.Modules[c.Request.Module].DB.Name]; ok {
 		tbName := console.CamelcaseToUnderscore(c.Request.Controller)
-		primary := tbName+"_id"
+		primary := tbName + "_id"
 		return fn(jsonPost, s, tbName, primary)
 	} else {
-		return Result{-2, "No database configuration"}
+		return Result{-101, "No database configuration"}
 	}
 }
 
-//new router register
+// New router register
 func NewController(module string, c interface{}) {
 	if _, ok := controllers[module]; !ok {
 		controllers[module] = make(map[string]reflect.Value)
